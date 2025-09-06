63431bad6677f9933f363eed436ebdb4e38f2943
Junghwan Park <park930919@gmail.com>
2025-09-06 20:52:36 +0900

    GameInstance, GameMode, PlayerController 클래스 추가

---
+++ KGUnrealPortfolio/Controller/KGPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "KGPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KGUNREALPORTFOLIO_API AKGPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ KGUnrealPortfolio/GameMode/KGGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KGUNREALPORTFOLIO_API AKGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
